cc2b8b449177778c47d4e80d2ce3ecf8e526b229
Benjamin Appiah <ben.appiah451@gmail.com>
2025-07-17 17:22:52 +0000

    Added files for pages and users

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Posts: React.FC = () => {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <p>This is the posts page.</p>
+    </div>
+  );
+}
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,8 @@
+export const Users: React.FC = () => {
+  return (
+    <div>
+      <h1>Users Page</h1>
+      <p>This is the users page of the application.</p>
+    </div>
+  );
+}
