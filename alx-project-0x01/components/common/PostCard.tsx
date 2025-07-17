74caf6239eb46a195fe49b2f837b76ea7a557b91
Benjamin Appiah <ben.appiah451@gmail.com>
2025-07-17 21:43:16 +0000

    Files added for tasl 2

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,8 +1,20 @@
-export const Postcard: React.FC = () => {
+import { PostProps } from "@/interfaces"
+
+
+const PostCard: React.FC<PostProps> = ({ title, body, userId, id }) => {
     return (
-        <div className="postcard">
-            <h2>Postcard Component</h2>
-            <p>This is a simple postcard component.</p>
-        </div>
+        <>
+            <div className = " max-w-xl ax-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadowduration-300">
+                <div className="mb-4">
+                    <h2 className="text-2xl font-semi-bold text-gray-800"> {title} </h2>
+                </div>
+                <p className="text-gray-600">{body}</p>
+                <div className="mt-4 flex-center justify-between text-sm text-gray-500"></div>
+                <span>User ID: {userId} </span>
+                <span>Post ID: {id} </span>
+            </div>
+        </>
     )
 }
+
+export default PostCard; 
---
+++ alx-project-0x01/interfaces/index.ts
@@ -0,0 +1,8 @@
+// PostCard Props Interface 
+
+export interface PostProps {
+	userId: number;
+	id: number;
+	title: string;
+	body: string;
+}
